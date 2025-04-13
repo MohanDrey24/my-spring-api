972940a6bf147530e4678b069045349d9e7c557f
Mohan Drey <mohantampon@gmail.com>
2025-04-13 19:11:03 +0800

    Add validation in create user dto

--- src/main/kotlin/com/example/my_spring_api/dto/request/CreateUserRequest.kt
+++ src/main/kotlin/com/example/my_spring_api/dto/request/CreateUserRequest.kt
@@ -1,6 +1,9 @@
 package com.example.my_spring_api.dto.request
 
+import jakarta.validation.constraints.NotBlank
+
 data class CreateUserRequest(
+    @field:NotBlank(message = "email is required mofo")
     val email: String,
     val password: String
 )
