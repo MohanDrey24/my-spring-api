e82c3483d6e797c4fa097eb490be21979dc604cd
Mohan Drey <mohantampon@gmail.com>
2025-04-13 19:12:34 +0800

    update user service

--- src/main/kotlin/com/example/my_spring_api/service/UserService.kt
+++ src/main/kotlin/com/example/my_spring_api/service/UserService.kt
@@ -2,11 +2,11 @@ package com.example.my_spring_api.service
 
 import com.example.my_spring_api.dto.request.CreateUserRequest
 import com.example.my_spring_api.entity.*
-import com.example.my_spring_api.repository.AccountRepository
-import com.example.my_spring_api.repository.UserRepository
+import com.example.my_spring_api.repository.*
 import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.server.ResponseStatusException
 
 @Service
@@ -16,10 +16,10 @@ class UserService(
     private val passwordEncoder: PasswordEncoder,
 ) {
 
-    fun createUser(request: CreateUserRequest) {
-        val existingUser = userRepository.findByEmail(request.email)
+    @Transactional
+    fun createUser(request: CreateUserRequest): Account {
 
-        if (existingUser != null) {
+        userRepository.findByEmail(request.email)?.let {
             throw ResponseStatusException(
                 HttpStatus.CONFLICT,
                 "User with email ${request.email} already exists"
@@ -32,5 +32,7 @@ class UserService(
         val password = passwordEncoder.encode(request.password)
         val newAccount = Account(hash = password, user = newUser)
         accountRepository.save(newAccount)
+
+        return newAccount
     }
 }
