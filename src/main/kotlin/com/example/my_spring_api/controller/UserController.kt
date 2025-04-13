46e98f350d276b8ab8e84558314823d0a91fdc27
Mohan Drey <mohantampon@gmail.com>
2025-04-13 19:12:29 +0800

    update user controller

--- src/main/kotlin/com/example/my_spring_api/controller/UserController.kt
+++ src/main/kotlin/com/example/my_spring_api/controller/UserController.kt
@@ -3,6 +3,7 @@ package com.example.my_spring_api.controller
 import com.example.my_spring_api.dto.request.CreateUserRequest
 import com.example.my_spring_api.dto.response.CreateUserResponse
 import com.example.my_spring_api.service.UserService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -19,19 +20,14 @@ class UserController (
     }
 
     @PostMapping("/signup")
-    fun signUp(@RequestBody request: CreateUserRequest): ResponseEntity<Any> {
-        return try {
-            val user = userService.createUser(request)
-
-            ResponseEntity.status(HttpStatus.CREATED).body(
-                CreateUserResponse(
-                    message = "User created successfully",
-                    userId = null,
-                )
+    fun signUp(@Valid @RequestBody request: CreateUserRequest): ResponseEntity<CreateUserResponse> {
+        val newAccount = userService.createUser(request)
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            CreateUserResponse(
+                message = "User created successfully",
+                userId = newAccount.user?.id,
             )
-        } catch (e: IllegalArgumentException) {
-            ResponseEntity.badRequest().body(e.message)
-        }
+        )
     }
 
 }
