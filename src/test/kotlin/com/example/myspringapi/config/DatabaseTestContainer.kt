a657818d171eb944e951f102d627364cd144503e
Mohan Drey <mohantampon@gmail.com>
2025-04-27 16:42:21 +0800

    add database test container

---
+++ src/test/kotlin/com/example/myspringapi/config/DatabaseTestContainer.kt
@@ -0,0 +1,31 @@
+package com.example.myspringapi
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@SpringBootTest
+@Testcontainers
+abstract class DatabaseTestContainer {
+
+    companion object {
+        @Container
+        // this should be fetched from application config using configuration config
+        val postgres = PostgreSQLContainer<Nothing>("postgres:16.2-alpine").apply {
+            withDatabaseName("testdb")
+            withUsername("test")
+            withPassword("test")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun configureProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgres::getJdbcUrl)
+            registry.add("spring.datasource.username", postgres::getUsername)
+            registry.add("spring.datasource.password", postgres::getPassword)
+        }
+    }
+}
